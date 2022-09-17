a35a6bd5e68f88dff7da3e3a1172e66c381be1de
bhushan <bhushanwagh6@gmail.com>
2022-09-18 00:12:26 +0530

    added: Mobile fallback

---
+++ src/components/MobileFallback.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const MobileFallback = () => {
+  return (
+    <div className='mobileApp' >
+        <h1>
+            Why'd you open a web app on mobile browser,
+            web apps are made for experiencing on desktops and laptops.
+            Why you're such a pathetic human being.
+            You're the reasons web developers are unhappy and stressed.
+            Please voluteer to make a world better place by killing yourself
+        </h1>
+    </div>
+  )
+}
+
+export default MobileFallback
